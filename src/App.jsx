6e2f7eb32dbf1e07609440843ed32c8d0a431f05
Srilekha Ghosh <ghoshsrilekha32@gmail.com>
2025-05-21 12:16:50 +0530

    quiz app

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import {useState} from "react";
+import "./App.css";
+import questions from "./contants/questions";
+import Question from  "./cmponents/question";
+import Result from  "./cmponents/Result";
+
+function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  // Keep all of the logic in App.jsx
+
+  const handleNextQuestion = (isCorrect) => {
+    setCurrentQuestion(currentQuestion + 1);
+    setUserAnswers([...userAnswers, isCorrect]);
+  };
+
+  const resetQuiz = () => {
+    setCurrentQuestion(0);
+    setUserAnswers([]);
+  };
+
+  return (
+    <div className="App">
+      <h1>World Quiz</h1>
+
+      {/* Questions Component */}
+      {currentQuestion < questions.length && (
+        <Question
+          question={questions[currentQuestion]}
+          onAnswerClick={handleNextQuestion}
+        />
+      )}
+      {currentQuestion === questions.length && (
+     <Result resetQuiz={resetQuiz}  userAnswers={userAnswers}   questions={questions}  />)}
+
+
+
+    </div>
+  );
+}
+
+export default App;
